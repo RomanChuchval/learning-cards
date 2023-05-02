3b6916ec128e893f26b6676f00d27596a8568be2
Roman Chuchval <rchuchval@gmail.com>
2023-05-02 12:32:48 +0300

    feature: add PackPage link to header, and add PageTitle component

--- src/common/components/app-header/AppHeader.tsx
+++ src/common/components/app-header/AppHeader.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import logo from "assets/img/logo.svg";
-import { Link, NavLink } from "react-router-dom";
-import AppBar from "@mui/material/AppBar/AppBar";
-import Toolbar from "@mui/material/Toolbar/Toolbar";
-import { Container } from "@mui/material";
-import { paths, SuperButton } from "common";
+import React from 'react';
+import logo from 'assets/img/logo.svg';
+import { Link, NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar/AppBar';
+import Toolbar from '@mui/material/Toolbar/Toolbar';
+import { Container } from '@mui/material';
+import { paths, SuperButton } from 'common';
 
 export const AppHeader = () => {
     return (
-        <AppBar color={"inherit"} position={"static"}>
+        <AppBar color={'inherit'} position={'static'}>
             <Container fixed>
-                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-                    <img src={logo} alt="app-logo" />
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <img src={logo} alt='app-logo' />
                     <NavLink to={paths.LOGIN}>login</NavLink>
                     <Link to={paths.REGISTER}>registration</Link>
                     <Link to={paths.CHECK_EMAIL}>check-email</Link>
@@ -19,13 +19,14 @@ export const AppHeader = () => {
                     <Link to={paths.FORGOT_PASSWORD}>forgot</Link>
                     <Link to={paths.PAGE_NOT_FOUND}>404</Link>
                     <Link to={paths.PROFILE}>Profile</Link>
+                    <Link to={paths.PACKS}>Packs</Link>
                     <SuperButton
                         callback={() => {}}
-                        width={"113"}
+                        width={'113'}
                         rounded={true}
-                        color={"primary"}
-                        textColor={"white"}
-                        name={"Sign In"}
+                        color={'primary'}
+                        textColor={'white'}
+                        name={'Sign In'}
                     />
                 </Toolbar>
             </Container>
