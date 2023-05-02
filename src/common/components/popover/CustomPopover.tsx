b8c6c78304fe5f49a738857a76d22859b641dcfa
Roman Chuchval <rchuchval@gmail.com>
2023-05-02 20:54:41 +0300

    feature: create CustomPopover

---
+++ src/common/components/popover/CustomPopover.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode } from 'react';
+import Popover from '@mui/material/Popover';
+
+type CustomPopoverPropsType = {
+    anchorEl: HTMLButtonElement | null;
+    handleClose: () => void;
+    children?: ReactNode;
+};
+
+export const CustomPopover: FC<CustomPopoverPropsType> = ({ handleClose, anchorEl, children }) => {
+    return (
+        <Popover
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+        >
+            {children}
+        </Popover>
+    );
+};
