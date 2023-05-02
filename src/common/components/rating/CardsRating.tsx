fd144b9fd854c87e030f4e72e75d57944d88714b
Adammm <adammm.web@gmail.com>
2023-05-02 17:37:18 +0300

    features: create rating

---
+++ src/common/components/rating/CardsRating.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Rating } from '@mui/material';
+
+type CardsRatingPropsType = {
+    defaultValue: number
+}
+
+export const CardsRating: React.FC<CardsRatingPropsType> = ({defaultValue}) => {
+    return <Rating name="half-rating-read" defaultValue={defaultValue} precision={0.5} readOnly />
+}
