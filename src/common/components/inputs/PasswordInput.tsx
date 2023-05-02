2195ee5008d6a36f72f4fdaed23e96d9687aa055
Roman Chuchval <rchuchval@gmail.com>
2023-05-02 11:21:55 +0300

    refactor: passwordInput formatting

--- src/common/components/inputs/PasswordInput.tsx
+++ src/common/components/inputs/PasswordInput.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from "react";
-import TextField from "@mui/material/TextField";
-import { Eye } from "common";
-import Box from "@mui/material/Box";
+import React, { FC } from 'react';
+import TextField from '@mui/material/TextField';
+import { Eye } from 'common';
+import Box from '@mui/material/Box';
 
 type PasswordInputType = {
     label: string;
@@ -9,33 +9,17 @@ type PasswordInputType = {
 
 export const PasswordInput: FC<PasswordInputType> = ({ label }) => {
     return (
-        <Box sx={{ mt: "30px", position: "relative" }}>
-            <TextField label={label} type={"password"} name={"email"} variant="standard" fullWidth={true} />
-            <Box sx={{ position: "absolute", right: "0", top: "10px" }}>
+        <Box sx={{ mt: '30px', position: 'relative' }}>
+            <TextField
+                label={label}
+                type={'password'}
+                name={'email'}
+                variant='standard'
+                fullWidth={true}
+            />
+            <Box sx={{ position: 'absolute', right: '0', top: '10px' }}>
                 <Eye />
             </Box>
         </Box>
     );
 };
-
-// <Box sx={{
-//     width: '100%',
-//     lineHeight: '24px',
-//     marginTop: '30px',
-//     position: 'relative',
-// }}>
-//     <TextField
-//         label={label}
-//         type={"password"}
-//         name={"email"}
-//         variant="standard"
-//         fullWidth={true}
-//     />
-//     <Box sx={{
-//         position: 'absolute',
-//         right: '0',
-//         top: '10px',
-//     }}>
-//         <Eye />
-//     </Box>
-// </Box>
