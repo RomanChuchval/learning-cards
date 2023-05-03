aa248dcba72b9849e0652c1df40b609dcccc2255
Roman Chuchval <rchuchval@gmail.com>
2023-05-03 17:44:31 +0300

    feature: add redirect to PacksPage if user logged in

--- src/features/auth/components/new-password/NewPassword.tsx
+++ src/features/auth/components/new-password/NewPassword.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Form, InfoMessage, PasswordInput } from 'common'
+import { Form, InfoMessage, PasswordInput, paths } from 'common'
+import { useAuth } from 'features/auth/useAuth'
+import { Navigate } from 'react-router-dom'
 
 export const NewPassword = () => {
+    const { profile } = useAuth()
+
+    if (profile) return <Navigate to={paths.PACKS} />
     return (
         <div>
             <Form title={'Create new password'} btnName={'Create new password'} onClick={() => {}}>
