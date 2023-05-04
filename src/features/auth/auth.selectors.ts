909c9f1cef2d67b1e586b78135c0c286e732b050
Adammm <adammm.web@gmail.com>
2023-05-05 00:01:03 +0300

    features: create pacs logic

--- src/features/auth/auth.selectors.ts
+++ src/features/auth/auth.selectors.ts
@@ -1,5 +1,6 @@
 import { RootState } from 'app/store'
 
 const profileSelector = (state: RootState) => state.auth.profile
+const userIdSelector = (state: RootState) => state.auth.profile?._id
 
-export { profileSelector }
+export { profileSelector, userIdSelector }
