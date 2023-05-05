72d304a29bcdd231d4175222ecfca808e7c43443
Roman Chuchval <rchuchval@gmail.com>
2023-05-05 11:53:41 +0300

    refactor: refactor redirect hook

--- src/features/auth/hooks/useRedirect.ts
+++ src/features/auth/hooks/useRedirect.ts
@@ -1,6 +1,5 @@
 import { useAppDispatch, useAppSelector } from 'app/hooks/hooks'
 import { redirectPathSelector } from 'features/auth/auth.selectors'
-import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { authActions } from 'features/auth/auth.slice'
 
@@ -9,10 +8,8 @@ export const useRedirect = () => {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
-    useEffect(() => {
-        if (redirectPath !== '/') {
-            navigate(redirectPath)
-            dispatch(authActions.setRedirectPath({ redirectPath: '/' }))
-        }
-    }, [redirectPath, navigate, dispatch])
+    if (redirectPath !== '/') {
+        navigate(redirectPath)
+        dispatch(authActions.setRedirectPath({ redirectPath: '/' }))
+    }
 }
