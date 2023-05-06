ab55d62d1b0012dcd19392b841c038f9f406cf93
Adammm <adammm.web@gmail.com>
2023-05-07 01:56:53 +0300

    refactor: Packs

--- src/features/packs/components/packs/Packs.tsx
+++ src/features/packs/components/packs/Packs.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect } from 'react'
 import Grid from '@mui/material/Grid'
-import { useAppFilter } from 'features/packs/hooks/useAppFilter'
+import { usePacksParamsFilter } from 'features/packs/hooks/usePacksParamsFilter'
 import { PacksHeader } from 'features/packs/components/packs-header/PacksHeader'
 import { PacksFilter } from 'features/packs/components/packs-filter/PacksFilter'
 import { PacksTable } from 'features/packs/components/packs-table/PacksTable'
+import { useAppDispatch } from 'app/hooks/hooks'
+import { packsAction, packsThunks } from 'features/packs/packs.slice'
 
 export const Packs = () => {
-    const { searchParams, filterHandler} = useAppFilter()
+    const { searchParams } = usePacksParamsFilter()
+    const dispatch = useAppDispatch()
     const paramsSearch = Object.fromEntries(searchParams)
-
+    console.log(paramsSearch)
     useEffect(() => {
-        filterHandler(paramsSearch)
+        dispatch(packsAction.setQueryParams({ params: { ...paramsSearch } }))
+        dispatch(packsThunks.getPacks())
     }, [])
 
     return (
         <Grid container spacing={2} sx={{ mt: '1px' }} rowSpacing={5} alignItems={'flex-end'}>
             <PacksHeader />
             <PacksFilter>
-                <PacksTable/>
+                <PacksTable />
             </PacksFilter>
+
         </Grid>
     )
 }
