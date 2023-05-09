bc107ba84bcfa3a4791aff9fe46b2002905bd96f
Adammm <adammm.web@gmail.com>
2023-05-10 00:40:12 +0300

    update: packs.slice

--- src/features/packs/packs.slice.ts
+++ src/features/packs/packs.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, isFulfilled, isPending, isRejected, PayloadAction } from '@reduxjs/toolkit'
 import {
     CreatePackModelType,
     GetPacksParamsType,
@@ -19,26 +19,28 @@ const slice = createSlice({
             max: '100',
             user_id: '',
             packName: ''
-        } as GetPacksParamsType
+        } as GetPacksParamsType,
+        isLoading: false
     },
     reducers: {
         setQueryParams: (state, actions: PayloadAction<{ params: GetPacksParamsType }>) => {
             state.params = { ...state.params, ...actions.payload.params }
         }
+
     },
     extraReducers: builder => {
         builder
             .addCase(getPacks.fulfilled, (state, action) => {
                 state.packs = action.payload.packs
             })
-            .addCase(getPacks.rejected, () => {
-                console.warn('rejectedGet')
+            .addMatcher(pendingPacks, state => {
+                state.isLoading = true
             })
-            .addCase(createPack.rejected, () => {
-                console.warn('rejectedCreate')
+            .addMatcher(fulfilledPacks, state => {
+                state.isLoading = false
             })
-            .addCase(removePack.rejected, () => {
-                console.warn('rejectedRemove')
+            .addMatcher(rejectedPacks, state => {
+                state.isLoading = false
             })
     }
 })
@@ -55,7 +57,7 @@ const getPacks = createAppAsyncThunk<{ packs: PacksResponseType }>(
 const createPack = createAppAsyncThunk<void, CreatePackModelType>(
     'packs/createPack',
     async (data, thunkAPI) => {
-        const {dispatch} = thunkAPI
+        const { dispatch } = thunkAPI
         await packsApi.createPack(data)
         dispatch(packsThunks.getPacks())
     }
@@ -63,7 +65,7 @@ const createPack = createAppAsyncThunk<void, CreatePackModelType>(
 const removePack = createAppAsyncThunk<void, string>(
     'packs/removePack',
     async (data, thunkAPI) => {
-        const {dispatch} = thunkAPI
+        const { dispatch } = thunkAPI
         await packsApi.removePack(data)
         dispatch(packsThunks.getPacks())
     }
@@ -71,10 +73,23 @@ const removePack = createAppAsyncThunk<void, string>(
 const updatePack = createAppAsyncThunk<void, UpdatePackModelType>(
     'packs/updatePack',
     async (data, thunkAPI) => {
-        const {dispatch} = thunkAPI
+        const { dispatch } = thunkAPI
         await packsApi.updatePack(data)
         dispatch(packsThunks.getPacks())
     }
+)
+
+const fulfilledPacks = isFulfilled(
+    getPacks,
+)
+const rejectedPacks = isRejected(
+    getPacks,
+)
+const pendingPacks = isPending(
+    getPacks,
+    createPack,
+    removePack,
+    updatePack
 )
 
 export const packsReducer = slice.reducer
