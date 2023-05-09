1af334035954179bc86e61fae2176b8a68d7432e
Adammm <adammm.web@gmail.com>
2023-05-10 00:39:57 +0300

    update: packs.selectors

--- src/features/packs/packs.selectors.ts
+++ src/features/packs/packs.selectors.ts
@@ -1,4 +1,7 @@
 import { RootState } from 'app/store'
 
-export const packsSelector = (state: RootState) => state.packs.packs
-export const paramsSelector = (state: RootState) => state.packs.params
+const packsSelector = (state: RootState) => state.packs.packs
+const paramsSelector = (state: RootState) => state.packs.params
+const isLoadingPacksSelector = (state: RootState) => state.packs.isLoading
+
+export { packsSelector, paramsSelector, isLoadingPacksSelector }
