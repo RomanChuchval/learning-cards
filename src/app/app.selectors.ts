f79b3bab1c2789f0f8e63119388585340bd60f67
Adammm <adammm.web@gmail.com>
2023-05-10 00:39:03 +0300

    update: app.selector

--- src/app/app.selectors.ts
+++ src/app/app.selectors.ts
@@ -3,5 +3,6 @@ import { RootState } from 'app/store'
 const errorSelector = (state: RootState) => state.app.error
 const infoMessageSelector = (state: RootState) => state.app.infoMessage
 const isInitializeSelector = (state: RootState) => state.app.isAppInitialized
+const isLoadingSelector = (state: RootState) => state.app.isLoading
 
-export { errorSelector, infoMessageSelector, isInitializeSelector }
+export { errorSelector, infoMessageSelector, isInitializeSelector, isLoadingSelector }
