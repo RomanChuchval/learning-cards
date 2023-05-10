6736e8fef3d85715ff386ba9fe9915a92a7ad9c0
Adammm <adammm.web@gmail.com>
2023-05-10 13:28:57 +0300

    refactor: useModalsForm

--- src/common/hooks/useModalsForm.ts
+++ src/common/hooks/useModalsForm.ts
@@ -1,7 +1,7 @@
 import { FormInputValues, useAppForm } from 'features/auth/hooks/useAppForm'
 
-export const useModalsForm = (onEditor: (data: FormInputValues) => void) => {
-    const { register, errors, reset, handleSubmit } = useAppForm(['textInput'])
+export const useModalsForm = (onEditor: (data: FormInputValues) => void, packName?: string) => {
+    const { register, errors, reset, handleSubmit } = useAppForm(['textInput'], packName)
     const error = errors.textInput?.message
 
     const editorHandler = (data: FormInputValues) => {
