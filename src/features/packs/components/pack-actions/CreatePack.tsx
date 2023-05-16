825f3a885d1baab46f5bb203585cc6db863b6606
Adammm <adammm.web@gmail.com>
2023-05-16 19:34:32 +0300

    update: CreatePack

--- src/features/packs/components/pack-actions/CreatePack.tsx
+++ src/features/packs/components/pack-actions/CreatePack.tsx
@@ -3,21 +3,23 @@ import React from 'react'
 import { EditorModal } from 'common/components/modals/EditorModal'
 import { useAppModals } from 'common/hooks/useAppModals'
 import { useEditorModals } from 'features/packs/hooks/useEditorModals'
+import { useApp } from 'app/hooks/useApp'
 
 export const CreatePack = () => {
     const { open, handleClose, handleOpen } = useAppModals()
-    const {createPack} = useEditorModals(handleClose)
+    const { createPack } = useEditorModals(handleClose)
+    const {isLoading} = useApp()
 
     return (
         <>
             <EditorModal title={'Add Pack'}
                          open={open}
                          onEditor={createPack} handleClose={handleClose}>
-                <SuperButton
-                    name={'Add new pack'}
-                    callback={handleOpen}
-                    rounded={true}
-                    textColor={'white'} />
+                <SuperButton name={'Add new pack'}
+                             callback={handleOpen}
+                             rounded={true}
+                             textColor={'white'}
+                             disable={isLoading} />
             </EditorModal>
         </>
     )
