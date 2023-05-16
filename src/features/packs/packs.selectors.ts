b4aba3e770103a3fe6325e35741b8973cb731212
Adammm <adammm.web@gmail.com>
2023-05-16 19:35:36 +0300

    update: packs.selectors

--- src/features/packs/packs.selectors.ts
+++ src/features/packs/packs.selectors.ts
@@ -2,6 +2,8 @@ import { RootState } from 'app/store'
 
 const packsSelector = (state: RootState) => state.packs.packs
 const paramsSelector = (state: RootState) => state.packs.params
+const packsErrorSelector = (state: RootState) => state.packs.error
+const packsInfoMessageSelector = (state: RootState) => state.packs.infoMessage
 const isLoadingPacksSelector = (state: RootState) => state.packs.isLoading
 
-export { packsSelector, paramsSelector, isLoadingPacksSelector }
+export { packsSelector, paramsSelector, isLoadingPacksSelector, packsErrorSelector, packsInfoMessageSelector }
