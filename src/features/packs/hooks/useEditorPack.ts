81f13d9f9260be5b99a990b0b77a790f8630bba8
Roman Chuchval <rchuchval@gmail.com>
2023-05-17 13:16:01 +0300

    refactor: rename hook

--- src/features/packs/hooks/useEditorPack.ts
+++ src/features/packs/hooks/useEditorPack.ts
@@ -3,30 +3,29 @@ import { CreatePackModelType, UpdatePackModelType } from 'features/packs/packs.a
 import { packsThunks } from 'features/packs/packs.slice'
 import { useAppDispatch } from 'app/hooks/useAppDispatch'
 
-export const useEditorModals = (handleClose: () => void, packId?:string) => {
+export const useEditorPack = (handleClose: () => void, packId?: string) => {
     const dispatch = useAppDispatch()
 
     const createPack = (data: FormInputValues) => {
         const payload: CreatePackModelType = {
             name: data.textInput,
-            private: data.private
+            private: data.private,
         }
         dispatch(packsThunks.createPack(payload))
         handleClose()
     }
     const removePack = () => {
-        if (packId)
-        dispatch(packsThunks.removePack(packId))
+        if (packId) dispatch(packsThunks.removePack(packId))
         handleClose()
     }
     const updatePack = (data: FormInputValues) => {
         const payload: UpdatePackModelType = {
             _id: packId || '',
             name: data.textInput,
-            private: data.private
+            private: data.private,
         }
         dispatch(packsThunks.updatePack(payload))
         handleClose()
     }
-    return {createPack, removePack, updatePack}
+    return { createPack, removePack, updatePack }
 }
