742a206812cbdd16f48fdba204128edbe3665879
Adammm <adammm.web@gmail.com>
2023-05-19 17:55:22 +0300

    update: store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,6 +4,7 @@ import { authReducer } from 'features/auth/auth.slice'
 import { packsReducer } from 'features/packs/packs.slice'
 import { cardsReducer } from 'features/cards/cards.slice'
 import { modalsReducer } from 'features/modals/modals.slice'
+import { learnReducer } from 'features/learn/learn.slice'
 
 export const store = configureStore({
     reducer: {
@@ -12,6 +13,7 @@ export const store = configureStore({
         packs: packsReducer,
         cards: cardsReducer,
         modals: modalsReducer,
+        learn: learnReducer
     },
 })
 
