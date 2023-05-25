748132f455c20eb682a9a1dfe925a4433de35481
Adammm <adammm.web@gmail.com>
2023-05-25 18:37:42 +0300

    updated: learn slice

--- src/features/learn/learn.slice.ts
+++ src/features/learn/learn.slice.ts
@@ -1,7 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, isFulfilled, isPending, isRejected, isRejectedWithValue, PayloadAction } from '@reduxjs/toolkit'
 import { cardsApi, CardType, GetCardsParamsType, UpdateCardGradeRequestType } from 'features/cards/cards.api'
 import { createAppAsyncThunk } from 'common/utils/createAppAsyncThunk'
 import { thunkErrorHandler } from 'common/utils/thunkErrorHandler'
+import { clearNotifyStateAction } from 'common/utils/clearNotifyStateAction'
 
 const slice = createSlice({
     name: 'learn',
@@ -11,36 +12,53 @@ const slice = createSlice({
         requestsCount: 0 as number,
         selectedPackId: '' as string,
         isLoading: false as boolean,
+        error: null as null | string
     },
     reducers: {
         setAnswersCount: state => {
-            state.answersCount = state.answersCount+1
+            state.answersCount = state.answersCount + 1
         },
         setRequestCounter: state => {
-            state.requestsCount = state.requestsCount+1
+            state.requestsCount = state.requestsCount + 1
         },
-        resetCounter: (state, action: PayloadAction<string>)  => {
+        resetCounter: (state, action: PayloadAction<string>) => {
             state.selectedPackId = action.payload
             state.answersCount = 0
-            if(state.requestsCount === 2) {
+            if (state.requestsCount === 2) {
                 state.requestsCount = 0
             }
         }
     },
     extraReducers: builder => {
-        builder.addCase(getSortCard.fulfilled, (state, action) => {
-            state.cards = action.payload.cards
-        })
-    },
+        builder
+            .addCase(clearNotifyStateAction, state => {
+                state.error = null
+            })
+            .addCase(getSortCard.fulfilled, (state, action) => {
+                state.cards = action.payload.cards
+            })
+            .addMatcher(pendingLearn, state => {
+                state.isLoading = true
+            })
+            .addMatcher(fulfilledLearn, state => {
+                state.isLoading = false
+            })
+            .addMatcher(rejectedLearn, state => {
+                state.isLoading = false
+            })
+            .addMatcher(rejectedWithValueLearn, (state, action) => {
+                state.error = action.payload
+            })
+    }
 })
 
-const getSortCard = createAppAsyncThunk<{cards: CardType[]}, string >(
+const getSortCard = createAppAsyncThunk<{ cards: CardType[] }, string>(
     'learn/getSortCard',
-    async(id, thunkApi) => {
-        const {rejectWithValue, dispatch, getState} = thunkApi
+    async (id, thunkApi) => {
+        const { rejectWithValue, dispatch, getState } = thunkApi
         try {
             const sortGrade = getState().learn.requestsCount < 2 ? '0grade' : '1grade'
-            const param:GetCardsParamsType = {
+            const param: GetCardsParamsType = {
                 cardsPack_id: id,
                 pageCount: '5',
                 sortCards: sortGrade
@@ -48,9 +66,8 @@ const getSortCard = createAppAsyncThunk<{cards: CardType[]}, string >(
             const res = await cardsApi.getCards(param)
             dispatch(learnActions.resetCounter(id))
             dispatch(learnActions.setRequestCounter())
-            return {cards: res.data.cards}
-        }
-        catch (e) {
+            return { cards: res.data.cards }
+        } catch (e) {
             const error = thunkErrorHandler(e)
             return rejectWithValue(error)
         }
@@ -58,17 +75,25 @@ const getSortCard = createAppAsyncThunk<{cards: CardType[]}, string >(
 const updateCardGrade = createAppAsyncThunk<void, UpdateCardGradeRequestType>(
     'learn/updateCardGrade',
     async (data, thunkApi) => {
-        const {rejectWithValue, dispatch} = thunkApi
+        const { rejectWithValue, dispatch, getState } = thunkApi
+        const state = getState().learn
         try {
             await cardsApi.updateCardGrade(data)
             dispatch(learnActions.setAnswersCount())
-        }
-        catch (e) {
+            if (state.answersCount === state.cards.length - 1) {
+                dispatch(learnThunks.getSortCard(state.selectedPackId))
+            }
+        } catch (e) {
             const error = thunkErrorHandler(e)
             return rejectWithValue(error)
         }
     }
 )
+
+const pendingLearn = isPending(getSortCard, updateCardGrade)
+const fulfilledLearn = isFulfilled(getSortCard, updateCardGrade)
+const rejectedLearn = isRejected(getSortCard, updateCardGrade)
+const rejectedWithValueLearn = isRejectedWithValue(getSortCard, updateCardGrade)
 
 export const learnReducer = slice.reducer
 export const learnActions = slice.actions
