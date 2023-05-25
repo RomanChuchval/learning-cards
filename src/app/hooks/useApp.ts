ec6dcfc10e54512ab9b8f7ff37ec2a6d0d377696
Adammm <adammm.web@gmail.com>
2023-05-25 18:37:04 +0300

    updated: useApp hook

--- src/app/hooks/useApp.ts
+++ src/app/hooks/useApp.ts
@@ -1,15 +1,20 @@
 import { isInitializeSelector, isLoadingSelector } from 'app/app.selectors'
 import { useAppSelector } from 'app/hooks/useAppSelector'
 import { isLoadingPacksSelector } from 'features/packs/packs.selectors'
+import { isLoadingLearnSelector } from 'features/learn/learn.selector'
 
 export const useApp = () => {
     const isAuthLoading = useAppSelector(isLoadingSelector)
     const isInitialize = useAppSelector(isInitializeSelector)
     const isPacksLoading = useAppSelector(isLoadingPacksSelector)
-    const isLoading = isAuthLoading || isPacksLoading
+    const isLearnLoading = useAppSelector(isLoadingLearnSelector)
+
+    const isLoadingApp = isAuthLoading || isPacksLoading
+    const isLoadingLearn = isAuthLoading || isLearnLoading
 
     return {
         isInitialize,
-        isLoading
+        isLoadingApp,
+        isLoadingLearn
     }
 }
