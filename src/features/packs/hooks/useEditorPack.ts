76cd74ee52ee06862e0c56b5feabeff59ef87f70
Adammm <adammm.web@gmail.com>
2023-05-26 00:39:50 +0300

    updated: useEditorPack hook

--- src/features/packs/hooks/useEditorPack.ts
+++ src/features/packs/hooks/useEditorPack.ts
@@ -5,14 +5,21 @@ import { useAppDispatch } from 'app/hooks/useAppDispatch'
 import { modalsAction } from 'features/modals/modals.slice'
 import { useAppSelector } from 'app/hooks/useAppSelector'
 import { packIdSelector } from 'features/modals/modals.selector'
+import { useState } from 'react'
+import { convertFileToBase64 } from 'common/utils/toBase64'
 
 export const useEditorPack = () => {
     const dispatch = useAppDispatch()
     const packId = useAppSelector(packIdSelector)
+    const [img, setImg] = useState('')
 
     const createPack = (data: FormInputValues) => {
+        if (data.packImg[0]) {
+            convertFileToBase64(data.packImg[0], setImg)
+        }
         const payload: CreatePackModelType = {
             name: data.textInput,
+            deckCover: img,
             private: data.private,
         }
         dispatch(packsThunks.createPack(payload))
@@ -23,13 +30,18 @@ export const useEditorPack = () => {
         dispatch(modalsAction.closeModal())
     }
     const updatePack = (data: FormInputValues) => {
+        if (data.packImg[0]) {
+            convertFileToBase64(data.packImg[0], setImg)
+        }
         const payload: UpdatePackModelType = {
             _id: packId || '',
             name: data.textInput,
             private: data.private,
+            deckCover: img
         }
+        console.log(data.packImg[0])
         dispatch(packsThunks.updatePack(payload))
         dispatch(modalsAction.closeModal())
     }
-    return { createPack, removePack, updatePack }
+    return { createPack, removePack, updatePack, setImg, img }
 }
