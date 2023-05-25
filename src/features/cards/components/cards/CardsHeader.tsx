1fd2cf0644f34edf1dc7dee397540f4c4c66e151
Adammm <adammm.web@gmail.com>
2023-05-25 19:55:53 +0300

    updated: CardsHeader (learn button)

--- src/features/cards/components/cards/CardsHeader.tsx
+++ src/features/cards/components/cards/CardsHeader.tsx
@@ -12,45 +12,48 @@ import { PopoverCards } from 'common/components/popover/PopoverCards'
 import { CreateCard } from 'features/cards/components/cards-actions/CreateCard'
 import { useApp } from 'app/hooks/useApp'
 import Skeleton from '@mui/material/Skeleton'
+import { SuperButton } from 'common'
+import { useLearn } from 'features/learn/hooks/useLearn'
 
 export const CardsHeader = () => {
-    const { packUserId, selectedPack } = useCards()
+    const { packUserId, selectedPack, cards } = useCards()
     const { authorizedUserId } = useAuth()
     const { anchorEl, closePopover, handleClick } = usePopover()
     const { isCardsLoading } = useApp()
+    const { learnHandler } = useLearn(selectedPack._id)
     const isMyPack = packUserId === authorizedUserId
 
     return (
         <>
             <Grid item md={6}>
-                {isCardsLoading ? (
-                    <Skeleton width={'150px'} height={'50px'} />
-                ) : isMyPack ? (
-                    <Box
-                        sx={{
+                {isCardsLoading
+                    ? <Skeleton width={'150px'} height={'50px'} />
+                    : isMyPack
+                        ? <Box sx={{
                             display: 'flex',
                             alignItems: 'center',
-                            gap: '5px',
-                        }}
-                    >
-                        <PageTitle title={selectedPack.name} />
-                        <IconButton onClick={handleClick} sx={{ height: '32px', p: '0' }}>
-                            <PendingIcon color={'primary'} />
-                        </IconButton>
-                    </Box>
-                ) : (
-                    <PageTitle title={selectedPack.name} />
-                )}
+                            gap: '5px'
+                        }}>
+                            <PageTitle title={selectedPack.name} />
+                            <IconButton onClick={handleClick} sx={{ height: '32px', p: '0' }}>
+                                <PendingIcon color={'primary'} />
+                            </IconButton>
+                        </Box>
+                        : <PageTitle title={selectedPack.name} />}
             </Grid>
             <Grid item md={6} display={'flex'} justifyContent={'flex-end'}>
-                {/*TODO Add learn*/}
-                {isMyPack ? <CreateCard /> : <h3>Learn Pack</h3>}
+                {isMyPack ? <CreateCard /> :
+                    <SuperButton name={'Learn Pack'}
+                                 rounded={true}
+                                 textColor={'white'}
+                                 callback={learnHandler}
+                                 disable={cards.length === 0} />}
             </Grid>
             <CustomPopover anchorEl={anchorEl} closePopover={closePopover}>
-                <PopoverCards
-                    packId={selectedPack._id}
-                    packName={selectedPack.name}
-                    handleClose={closePopover}
+                <PopoverCards cards={cards.length}
+                              packId={selectedPack._id}
+                              packName={selectedPack.name}
+                              handleClose={closePopover}
                 />
             </CustomPopover>
         </>
