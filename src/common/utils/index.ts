3eb2aec31e02746263aee2e87e7ec423b14f83e1
Adammm <adammm.web@gmail.com>
2023-05-26 18:31:46 +0300

    create: index for common utils

---
+++ src/common/utils/index.ts
@@ -0,0 +1,4 @@
+export { clearNotifyStateAction } from './clearNotifyStateAction'
+export { createAppAsyncThunk } from './createAppAsyncThunk'
+export { thunkErrorHandler } from './thunkErrorHandler'
+export { convertFileToBase64 } from './toBase64'
