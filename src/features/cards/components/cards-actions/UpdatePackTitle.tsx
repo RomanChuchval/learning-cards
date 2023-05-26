a5f7a557ebe1310b1bc5bfb718b1bb6bc8f0ed6a
Adammm <adammm.web@gmail.com>
2023-05-26 18:28:22 +0300

    refactor: index for UpdatePackTitle

--- src/features/cards/components/cards-actions/UpdatePackTitle.tsx
+++ src/features/cards/components/cards-actions/UpdatePackTitle.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import BorderColorIcon from '@mui/icons-material/BorderColor'
 import ListItemText from '@mui/material/ListItemText'
-import { useAppModals } from 'common/hooks/useAppModals'
 import ListItemButton from '@mui/material/ListItemButton'
 import { AppModal } from 'features/modals/components/AppModal'
 import { PacksModalForm } from 'features/packs/components/modals/PacksModalForm'
 import { useEditorPack } from 'features/packs/hooks/useEditorPack'
+import { useAppModals } from 'common'
 
 type UpdatePackTitlePropsType = {
     packId: string
@@ -15,7 +15,7 @@ type UpdatePackTitlePropsType = {
 export const UpdatePackTitle: React.FC<UpdatePackTitlePropsType> = ({ packId, packName }) => {
     const { showUpdateModal, openUpdateModal, handleClose, selectedPackId } = useAppModals({
         packId,
-        packName,
+        packName
     })
     const { updatePack, img, setImg } = useEditorPack()
     return (
@@ -29,9 +29,8 @@ export const UpdatePackTitle: React.FC<UpdatePackTitlePropsType> = ({ packId, pa
             <AppModal
                 title={'Create Card'}
                 open={selectedPackId === packId && showUpdateModal}
-                handleClose={handleClose}
-            >
-                <PacksModalForm onSubmit={updatePack} handleClose={handleClose} img={img} setImg={setImg}/>
+                handleClose={handleClose}>
+                <PacksModalForm onSubmit={updatePack} handleClose={handleClose} img={img} setImg={setImg} />
             </AppModal>
         </>
     )
