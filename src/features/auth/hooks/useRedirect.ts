aa4281ae1e766630505e669cb350c64e6cabb141
Roman Chuchval <rchuchval@gmail.com>
2023-05-26 17:30:55 +0300

    refactor: add common action for clear redirect path

--- src/features/auth/hooks/useRedirect.ts
+++ src/features/auth/hooks/useRedirect.ts
@@ -1,8 +1,8 @@
 import { redirectPathSelector } from 'features/auth/auth.selectors'
 import { useNavigate } from 'react-router-dom'
-import { authActions } from 'features/auth/auth.slice'
 import { useAppSelector } from 'app/hooks/useAppSelector'
 import { useAppDispatch } from 'app/hooks/useAppDispatch'
+import { clearRedirectPathAction } from 'common/utils/clearRedirectPathAction'
 
 export const useRedirect = () => {
     const redirectPath = useAppSelector(redirectPathSelector)
@@ -11,6 +11,6 @@ export const useRedirect = () => {
 
     if (redirectPath !== '/') {
         navigate(redirectPath)
-        dispatch(authActions.clearRedirectPath())
+        dispatch(clearRedirectPathAction())
     }
 }
