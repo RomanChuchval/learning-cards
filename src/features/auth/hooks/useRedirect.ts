cf553a60b235574b1d17256e7d25889b82719a67
Adammm <adammm.web@gmail.com>
2023-05-26 18:29:37 +0300

    refactor: index for useRedirect

--- src/features/auth/hooks/useRedirect.ts
+++ src/features/auth/hooks/useRedirect.ts
@@ -1,8 +1,7 @@
 import { redirectPathSelector } from 'features/auth/auth.selectors'
 import { useNavigate } from 'react-router-dom'
 import { authActions } from 'features/auth/auth.slice'
-import { useAppSelector } from 'app/hooks/useAppSelector'
-import { useAppDispatch } from 'app/hooks/useAppDispatch'
+import { useAppSelector, useAppDispatch } from 'app'
 
 export const useRedirect = () => {
     const redirectPath = useAppSelector(redirectPathSelector)
