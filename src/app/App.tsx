5058cec6ada2ea43ed87226985d4fd87c68917e1
Roman Chuchval <rchuchval@gmail.com>
2023-05-28 12:24:03 +0300

    refactor: update App imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import { AppHeader } from 'common/components/app-header/AppHeader'
 import Container from '@mui/material/Container'
 import { authThunks } from 'features/auth/auth.slice'
-import { useAppDispatch } from 'app/hooks/useAppDispatch'
-import { AppNotify } from 'common/components/app-notify/AppNotify'
-import { useAppNotify } from 'app/hooks/useAppNotify'
-import { AppPreloader } from 'common/components/preloader/AppPreloader'
 import LinearProgress from '@mui/material/LinearProgress/LinearProgress'
-import { useApp } from 'app/hooks/useApp'
+import { AppHeader, AppNotify, AppPreloader } from 'common'
+import { useApp, useAppDispatch, useAppNotify } from './hooks'
 
 export const App = () => {
     const dispatch = useAppDispatch()
-    const {isInitialize, isLoadingApp} = useApp()
+    const { isInitialize, isLoadingApp } = useApp()
     useAppNotify()
 
     useEffect(() => {
