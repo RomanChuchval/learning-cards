6821a2c3143ee1472e4db93f318d34e36bacb37e
Adammm <adammm.web@gmail.com>
2023-06-02 00:44:40 +0300

    refactor: AppNotify

--- src/common/components/app-notify/AppNotify.tsx
+++ src/common/components/app-notify/AppNotify.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
+import { useAppNotify } from 'app'
 
 export const AppNotify = () => {
+    useAppNotify()
     return (
         <ToastContainer
             position='bottom-right'
