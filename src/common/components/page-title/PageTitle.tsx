33f8e0ef225557750b76fc81745f8cded5060667
Adammm <adammm.web@gmail.com>
2023-06-01 19:16:02 +0300

    refactor: PageTitle

--- src/common/components/page-title/PageTitle.tsx
+++ src/common/components/page-title/PageTitle.tsx
@@ -9,7 +9,7 @@ type PageTitlePropsType = {
 export const PageTitle: FC<PageTitlePropsType> = memo(({ title }) => {
     return (
         <Box component={'h3'}>
-            <Typography sx={{ fontWeight: '600', fontSize: '22px' }}>{title}</Typography>
+            <Typography sx={{ fontWeight: '600', fontSize: '22px', wordBreak: 'break-all' }}>{title}</Typography>
         </Box>
     )
 })
