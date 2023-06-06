f0ef3b95f760be914ef0219c713b2611ddb494ba
Adammm <adammm.web@gmail.com>
2023-06-06 18:34:25 +0300

    refactor: modals.selector

--- src/features/modals/modals.selector.ts
+++ src/features/modals/modals.selector.ts
@@ -4,6 +4,7 @@ const modalStateSelector = (state: RootState) => ({
     packName: state.modals.modalState.packName,
     question: state.modals.modalState.question === ' ' ? '' : state.modals.modalState.question,
     answer: state.modals.modalState.answer === ' ' ? '' : state.modals.modalState.answer,
+    packImg: state.modals.modalState.packImg,
     questionImg: state.modals.modalState.questionImg,
     answerImg: state.modals.modalState.answerImg,
 })
