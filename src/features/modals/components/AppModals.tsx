98cff82e7938b85db3d09c2ee920aef462a29e10
Adammm <adammm.web@gmail.com>
2023-06-08 21:13:20 +0300

    refactor: AppModals

--- src/features/modals/components/AppModals.tsx
+++ src/features/modals/components/AppModals.tsx
@@ -5,78 +5,40 @@ import { RemoveModal } from 'features/modals/components/common/RemoveModal'
 import { RemoveModalMessage } from 'features/modals/components/common/RemoveModalMessage'
 import { AppModal } from 'features/modals/components/common/AppModal'
 import { useAppModals } from 'features/modals/hooks/useAppModals'
-import { useEditorCards } from 'features/cards/hooks/useEditorCards'
-import { useEditorPack } from 'features/packs/hooks/useEditorPack'
 
 export const AppModals = () => {
     const {
         isModalOpen,
-        modalAction,
         modalConfig,
         handleClose,
-        cardQuestion,
-        cardQuestionImg,
-        packName,
-        packCover,
-        withRedirect,
+        defaultPackCover,
+        setImg
     } = useAppModals()
-    const { createCard, updateCard, removeCard } = useEditorCards()
-    const { createPack, updatePack, removePack, img, setImg } = useEditorPack(
-        packCover,
-        withRedirect
-    )
-
     return (
         <AppModal title={modalConfig.title} open={isModalOpen} handleClose={handleClose}>
-            {modalAction === 'createPack' && (
-                <PacksModalForm
-                    onSubmit={createPack}
-                    handleClose={handleClose}
-                    img={img}
-                    setImg={setImg}
+            {modalConfig.variant === 'packModal'
+            && <PacksModalForm
+                onSubmit={modalConfig.action}
+                handleClose={handleClose}
+                img={defaultPackCover}
+                setImg={setImg}
+            />
+            }
+            {modalConfig.variant === 'cardModal'
+            && <CardsModalForm onSubmit={modalConfig.action} handleClose={handleClose} />}
+            {modalConfig.variant === 'removeModal'
+            && <RemoveModal
+                modalTitle={modalConfig.title}
+                handleClose={handleClose}
+                onRemove={modalConfig.action as () => void}
+            >
+                <RemoveModalMessage
+                    whatToDelete={modalConfig.whatToDelete || ''}
+                    entityName={modalConfig.entityName || ''}
+                    entityImage={modalConfig.entityImage}
                 />
-            )}
-            {modalAction === 'updatePack' && (
-                <PacksModalForm
-                    onSubmit={updatePack}
-                    handleClose={handleClose}
-                    img={img}
-                    setImg={setImg}
-                    defaultImg={packCover}
-                />
-            )}
-            {modalAction === 'removePack' && (
-                <RemoveModal
-                    modalTitle={modalConfig}
-                    handleClose={handleClose}
-                    onRemove={removePack}
-                >
-                    <RemoveModalMessage
-                        entityName={packName}
-                        entityImage={packCover}
-                        whatToDelete={'This pack'}
-                    />
-                </RemoveModal>
-            )}
-            {modalAction === 'createCard' && (
-                <CardsModalForm onSubmit={createCard} handleClose={handleClose} />
-            )}
-            {modalAction === 'updateCard' && (
-                <CardsModalForm onSubmit={updateCard} handleClose={handleClose} />
-            )}
-            {modalAction === 'removeCard' && (
-                <RemoveModal
-                    modalTitle={modalConfig}
-                    handleClose={handleClose}
-                    onRemove={removeCard}
-                >
-                    <RemoveModalMessage
-                        entityName={cardQuestion}
-                        entityImage={cardQuestionImg}
-                        whatToDelete={'This card'}
-                    />
-                </RemoveModal>
-            )}
+            </RemoveModal>
+            }
         </AppModal>
     )
 }
