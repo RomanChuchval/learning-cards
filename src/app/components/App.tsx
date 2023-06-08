94c1147787e020058c583634a9649a1bbb5b1e30
Adammm <adammm.web@gmail.com>
2023-06-08 12:37:25 +0300

    refactor: AppModals

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AppHeader, AppNotify } from 'common'
 import { Main } from 'app/components/Main'
+import { AppModals } from 'features/modals/components/AppModals'
 
 export const App = () => {
     return (
@@ -8,6 +9,7 @@ export const App = () => {
             <AppHeader />
             <Main />
             <AppNotify />
+            <AppModals />
         </>
     )
 }
