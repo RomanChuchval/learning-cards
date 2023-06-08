5830f1005c80cac628438e3af963a65df7ef13ed
Adammm <adammm.web@gmail.com>
2023-06-08 12:38:24 +0300

    refactor: Cards

--- src/features/packs/components/packs/Packs.tsx
+++ src/features/packs/components/packs/Packs.tsx
@@ -1,58 +1,15 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import Grid from '@mui/material/Grid'
 import { PacksHeader } from 'features/packs/components/packs/PacksHeader'
 import { PackMain } from 'features/packs/components/packs/PackMain'
-import { PacksModalForm } from 'features/modals/components/packs-modals/PacksModalForm'
-import { AppModal } from 'features/modals/components/common/AppModal'
-import { useEditorPack } from 'features/packs/hooks/useEditorPack'
-import { RemoveModalMessage } from 'features/modals/components/common/RemoveModalMessage'
-import { useAppModals } from 'features/modals/hooks/useAppModals'
-import { RemoveModal } from 'features/modals/components/common/RemoveModal'
 
 export const Packs = () => {
-    const { packCover, packName, handleClose, isModalOpen, modalAction, modalTitle } =
-        useAppModals()
-    const { createPack, updatePack, removePack, img, setImg } = useEditorPack(packCover)
-
-    const onRemovePack = useCallback(() => {
-        removePack(false)
-    }, [removePack])
-
     return (
         <>
             <Grid container spacing={2} sx={{ mt: '1px' }} rowSpacing={5} alignItems={'flex-end'}>
                 <PacksHeader />
                 <PackMain />
             </Grid>
-
-            <AppModal title={modalTitle} open={isModalOpen} handleClose={handleClose}>
-                {modalAction === 'createPack' && (
-                    <PacksModalForm
-                        onSubmit={createPack}
-                        handleClose={handleClose}
-                        img={img}
-                        setImg={setImg}
-                    />
-                )}
-                {modalAction === 'updatePack' && (
-                    <PacksModalForm
-                        onSubmit={updatePack}
-                        handleClose={handleClose}
-                        img={img}
-                        setImg={setImg}
-                        defaultImg={packCover}
-                    />
-                )}
-                {modalAction === 'removePack' && (
-                    <RemoveModal
-                        modalTitle={modalTitle}
-                        handleClose={handleClose}
-                        onRemove={onRemovePack}
-                    >
-                        <RemoveModalMessage entityName={packName} whatToDelete={'This pack'} />
-                    </RemoveModal>
-                )}
-            </AppModal>
         </>
     )
 }
